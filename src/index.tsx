1669788f3c5130b34c53e641526b3c03f010111a
esteficodes <estefaniaquevedolusby@gmail.com>
2023-03-07 11:39:56 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const container = document.getElementById('app-root')!
+const root = createRoot(container)
+root.render(<h1>Let's the rock the hell out of this project!</h1>)
+
+import './index.css'
